de27df7288f799ad454ad77fbd3364957b299bd5
bado14junior <juniorbadoloofficiel@gmail.com>
2025-08-29 22:50:14 +0000

    service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+
+const cacheName = 'jbpg-cache-v1';
+const assets = [
+  './',
+  './index.html',
+  './style.css',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
