d55636065cba8666976d5046e2fe9a2fd925e392
bado14junior <juniorbadoloofficiel@gmail.com>
2025-09-04 07:23:46 +0000

    App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+import Chat from "./Chat";
+
+function App() {
+  return (
+    <div>
+      <h1>Ma messagerie Firebase</h1>
+      <Chat />
+    </div>
+  );
+}
+
+export default App;
