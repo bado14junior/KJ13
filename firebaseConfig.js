6543820d370c7f5025101589e72c256a96759e3e
bado14junior <juniorbadoloofficiel@gmail.com>
2025-09-04 07:14:06 +0000

    firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBbkxs-YdWui8fVusH0yTibL5GlHqkP_uA",
+  authDomain: "jb-plateforme-global-1e63f.firebaseapp.com",
+  projectId: "jb-plateforme-global-1e63f",
+  storageBucket: "jb-plateforme-global-1e63f.firebasestorage.app",
+  messagingSenderId: "877337046664",
+  appId: "1:877337046664:web:63aa60ea9fd853afeb393d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
